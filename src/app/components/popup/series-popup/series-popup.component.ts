1e22bc344e83aa490d3dcaa70ddbf51a2a5c70f5
pizza61 <34103636+pizza61@users.noreply.github.com>
2020-07-08 14:19:01 +0200

    Series popup chart

--- src/app/components/popup/series-popup/series-popup.component.ts
+++ src/app/components/popup/series-popup/series-popup.component.ts
@@ -3,18 +3,30 @@ import { Media, SeriesService } from 'src/app/services/series.service';
 import { StatsService } from 'src/app/services/stats.service';
 import { ActivityDay } from 'src/app/interfaces/activity-day';
 import { MonthPipe } from 'src/app/pipes/month.pipe';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-series-popup',
   templateUrl: './series-popup.component.html',
   styleUrls: ['./series-popup.component.scss'],
-  providers: [MonthPipe]
+  providers: [MonthPipe],
+  animations: [
+    trigger(
+      'w', [
+        transition(':enter', [
+          style({ opacity: 0 }),
+          animate('400ms ease-in-out', style({ opacity: 1 }))
+        ])
+      ]
+    )
+  ]
 })
 export class SeriesPopupComponent implements OnInit {
   @Input() data: Media;
   @Output() close = new EventEmitter();
 
   current: ActivityDay[];
+  nodata: boolean = false;
 
   constructor(private seriesService: SeriesService, private statsService: StatsService, private monthPipe: MonthPipe) { }
 
@@ -35,7 +47,11 @@ export class SeriesPopupComponent implements OnInit {
         ...x,
         topText: x.day.date + '.' + this.monthPipe.transform(x.day.month) + '.' + x.day.year, // TODO: date localization
         bottomText: x.day.weekday
-      }));
+      })).reverse();
+
+      if(this.current.length == 0) {
+        this.nodata = true;
+      }
     })
   }
 }
