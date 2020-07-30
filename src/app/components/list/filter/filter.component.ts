7620e9149a1ccf3532f379033d0eb38558d96fe0
pizza61 <34103636+pizza61@users.noreply.github.com>
2020-07-30 20:10:11 +0200

    Added filter (de)select all and select animation

--- src/app/components/list/filter/filter.component.ts
+++ src/app/components/list/filter/filter.component.ts
@@ -1,10 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Filter } from 'src/app/interfaces/filters';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
+  animations: [
+    trigger(
+      'checkAnimation', [
+        transition(':enter', [
+          style({ opacity: 0 }),
+          animate('100ms ease-in', style({ opacity: 1}))
+        ]),
+        transition(':leave', [
+          animate('100ms ease-out', style({ opacity: 0 }))
+        ])
+      ]
+    )
+  ]
 })
 export class FilterComponent implements OnInit {
   @Input() filter: Filter;
@@ -13,6 +27,14 @@ export class FilterComponent implements OnInit {
 
   ngOnInit(): void {
     
+  }
+
+  selectAll() {
+    if(this.filter.checked.length == this.filter.values.length) { // already selected all
+      this.filter.checked = [];
+    } else {
+      this.filter.checked = this.filter.values.map(x => x.key);
+    }
   }
 
   check(key: string) {
