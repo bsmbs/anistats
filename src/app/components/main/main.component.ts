ec406d6fa1b78b794a73e8651cd155c068d7e0c6
pizza61 <34103636+pizza61@users.noreply.github.com>
2020-11-14 12:02:06 +0100

    Added help

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -18,6 +18,15 @@ import { UserService } from '../../services/user.service';
         animate('200ms ease-out', style({ opacity: 0, bottom: 0 }))
       ]
       )
+    ]),
+    trigger('help', [
+      transition(':enter', [
+        style({ opacity: 0, top: '24px' }),
+        animate('200ms ease-out', style({ opacity: 1, top: '40px' }))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-out', style({ opacity: 0, top: '24px'}))
+      ])
     ])
   ]
 })
@@ -25,6 +34,8 @@ export class MainComponent implements OnInit {
   errorVisible: boolean = false;
   username: string = '';
   loading: boolean = false;
+
+  showHelp: boolean = false;
 
   constructor(private router: Router, private user: UserService) { }
 
@@ -46,5 +57,9 @@ export class MainComponent implements OnInit {
       this.errorVisible = true;
       setTimeout(() => { this.errorVisible = false; }, 2000);
     });
+  }
+
+  toggleHelp(): void {
+    this.showHelp = !this.showHelp;
   }
 }
